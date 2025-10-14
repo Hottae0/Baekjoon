feba5609ed70b48f303fdb620615f1be7217b6e0
하태영 <116103629+Hottae0@users.noreply.github.com>
2025-10-14 20:42:16 +0900

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -17,8 +17,9 @@ def get_solved_problems(user_id: str):
     """
     print(f"'{user_id}'님의 맞은 문제 목록을 가져오는 중...")
     url = f"https://www.acmicpc.net/user/{user_id}"
-    headers = {"User-Agent": "Mozilla/5.0"}
-    
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
+}    
     try:
         response = requests.get(url, headers=headers)
         if response.status_code != 200:
