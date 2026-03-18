077da309260f0d8df5df61c60be566eeb7acad19
하태영 <116103629+Hottae0@users.noreply.github.com>
2026-03-19 00:44:17 +0900

    [Bronze II] Title: 공 바꾸기, Time: 40 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
@@ -1,18 +1,10 @@
 N, M = map(int, input().split())
 
-list = []
-for i in range (N):
-    list.append(i + 1)
+arr = list(range(1, N + 1))
 
-for i in range(M):
+for _ in range(M):
     a, b = map(int, input().split())
 
-    a -= 1
-    b -= 1
+    arr[a - 1], arr[b - 1] = arr[b - 1], arr[a - 1]
 
-    tmp = list[a]
-    list[a] = list[b]
-    list[b] = tmp
-
-for i in list:
-    print(i, end = " ")
+print(*arr)
