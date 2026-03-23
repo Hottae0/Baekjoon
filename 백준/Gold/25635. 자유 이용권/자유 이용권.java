a7fa9e88d749bea86f30b3b9f75e5beb03aca1d0
하태영 <116103629+Hottae0@users.noreply.github.com>
2026-03-24 04:18:17 +0900

    [Gold IV] Title: 자유 이용권, Time: 284 ms, Memory: 25812 KB -BaekjoonHub

--- 백준/Gold/25635. 자유 이용권/자유 이용권.java
+++ 백준/Gold/25635. 자유 이용권/자유 이용권.java
@@ -7,20 +7,17 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
-
-        PriorityQueue<Long> que = new PriorityQueue<>(Comparator.reverseOrder());
-
+        
         long sum = 0;
-
+        long max = 0;
+        
         StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i = 0 ; i < N ; i++){
             long tmp = Long.parseLong(st.nextToken());
             sum += tmp;
-
-            que.add(tmp);
+            max = Math.max(max, tmp);
         }
 
-        long max = que.peek();
         long rest = sum - max;
 
         if(rest < max){ // 가장 큰 숫자가 다른 숫자를 모두 커버 가능함
